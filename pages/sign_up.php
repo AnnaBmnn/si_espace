b9e929808fa3e955d591395eabb53bc3abd977f5
AnnaBmnn <annabaumann@laposte.net>
2017-03-28 16:22:13 +0200

    add sign in and sign up pages

---
+++ pages/sign_up.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <title>Title</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+</head>
+
+<body>
+    <div class="login-box">
+        <div class="left">
+        </div>
+        <div class="right">
+            <div class="log">
+                <a class="log1" href="sign_in.php">SIGN IN</a>
+                <a class="log2" href="#">SIGN UP</a>
+            </div>
+            <form action="#" method="post">
+                <div>
+                    <label for="name">Choose a username</label>
+                    <input type="text" name="user" value="" placeholder="JohnSmith" id="user">
+                </div>
+                <div>
+                    <label for="email">Enter your email</label>
+                    <input type="text" name="user" value="" placeholder="JohnSmith" id="user">
+                </div>
+                <div>
+                    <label for="pswd">Password</label>
+                    <input type="password" name="pswd" value="" id="pswd" placeholder="*****">
+                </div>
+                <div>
+                    <label for="pswd">Confirm the password</label>
+                    <input type="password" name="pswd" value="" id="pswd" placeholder="*****">
+                </div>
+                <input type="submit">
+            </form>
+        </div>
+</body>
+
+</html>
