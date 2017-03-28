35ca53c1d121563bf40cd09ebcd3b17c308c9567
MatthiasNT <matthias.ngotrung@gmail.com>
2017-03-28 15:55:14 +0200

    Galery

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,4 @@ $(window).load(function() {
   
   $('body').removeClass('preload');
   
-  $('.hide_left').on('click', function(){
-    $(this).toggleClass('displayed');
-    $('.informations').toggle();
-    $(this).html('>');
-  });
-  
 });
