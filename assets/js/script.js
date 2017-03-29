c006fbc51d4f7e3a4d2005588e5b3a9db1787e17
AnnaBmnn <annabaumann@laposte.net>
2017-03-29 17:32:19 +0200

    trad jquery -> js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,66 @@
-$(window).load(function() {
-  
-  $('body').removeClass('preload');
+window.addEventListener('load', function() {
+    var body = document.querySelector('body'),
+    loader = document.querySelector('.loader'),
+    header = document.querySelector('header'),
+    content = document.querySelector('#content'),
+    back_top = document.querySelector('.back_top');
     
-  $('.loader').hide()
-  $('header').removeClass('preload');
-  $('#content').removeClass('preload');
-  
-  $(window).scroll(function() {
-    if ($(this).scrollTop() >= 1800){
-      $('.back_top').show();
-    }else{
-      $('.back_top').hide(); 
+    body.classList.remove('preload');
+    loader.style.display = 'none';
+    header.classList.remove('preload');
+    content.classList.remove('preload');
+
+    window.addEventListener('scroll', function(){
+        if (document.body.scrollTop >= 1800){
+            back_top.style.display = 'inherit';
+        }else{
+            back_top.style.display = 'none';
+        }
+    });
+    
+    back_top.addEventListener('click',function(){
+        scrollTo(document.body, 0, 1000);
+    });
+
+function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+        change = to - start,
+        increment = 20;
+
+    var animateScroll = function(elapsedTime) {        
+        elapsedTime += increment;
+        var position = easeInOut(elapsedTime, start, change, duration);                        
+        element.scrollTop = position; 
+        if (elapsedTime < duration) {
+            setTimeout(function() {
+                animateScroll(elapsedTime);
+            }, increment);
+        }
+    };
+
+    animateScroll(0);
+}
+
+function easeInOut(currentTime, start, change, duration) {
+    currentTime /= duration / 2;
+    if (currentTime < 1) {
+        return change / 2 * currentTime * currentTime + start;
     }
-  });
+    currentTime -= 1;
+    return -change / 2 * (currentTime * (currentTime - 2) - 1) + start;
+}
+    
+    
+    
+    
+    /*
 
   $('.back_top').click(function(){
     $('body,html').animate({
       scrollTop : 1600
     }, 1000);
-  });
+  });*/
   
-});
-
 var image_container = document.querySelectorAll('.img_plus'),
     body = document.querySelector('body'),
     photo_liked = [];
@@ -44,7 +83,10 @@ for(var i=0; i<image_container.length; i++){
             while( j < photo_liked.length ){
                 if(photo_liked[j]== this.dataset.id){
                     photo_liked.splice(j,1);
-                    j = photo_liked.length;
+                    j = photo_liked.length+1;
+                }
+                else {
+                    j++ ;
                 }
             }
         }
@@ -83,3 +125,7 @@ function create_data_string(tab){
     }
     return data_string ;
 }
+    
+   
+});
+
